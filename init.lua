20c4bf38641cbef98ad6e1068d334f48bd811096
golonzovsky <2477950+golonzovsky@users.noreply.github.com>
2024-06-21 11:33:17 +0200

    onedark and typescript-tools.nvim

--- init.lua
+++ init.lua
@@ -605,6 +605,12 @@ require('lazy').setup({
     end,
   },
 
+    {
+      'pmizio/typescript-tools.nvim',
+      dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+      opts = {},
+    },
+
   -- LSP Plugins
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
@@ -1026,6 +1032,15 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'navarasu/onedark.nvim',
+    config = function()
+      require('onedark').setup {
+        style = 'warmer',
+      }
+    end,
+  },
+
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
@@ -1039,12 +1054,14 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-moon'
-      --vim.cmd.colorscheme 'onedark'
+      -- vim.cmd.colorscheme 'tokyonight-moon'
+      vim.cmd.colorscheme 'onedark'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
-      vim.cmd.hi 'Normal guibg=NONE ctermbg=NONE'
+
+      -- below makes the background transparent
+      -- vim.cmd.hi 'Normal guibg=NONE ctermbg=NONE'
     end,
   },
 
